d3468b7d271ba4080229c049a10e8755ce6b55ae
peironggg <wupeirong294@gmail.com>
2018-10-21 22:25:56 +0800

    Log In & Register Screen

---
+++ Shopee/WelcomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Shopee
+//
+//  Created by Wu Peirong on 21/10/18.
+//  Copyright © 2018 Wu Peirong. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    @IBAction func logInPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+            if error != nil {
+                let errorMessage = error?.localizedDescription
+                let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                print("Log In Successful")
+            }
+        }
+    }
+    
+}
+
