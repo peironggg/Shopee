795f806530b97755c01974e686bc794e6cad3d2e
peironggg <wupeirong294@gmail.com>
2018-10-24 22:18:28 +0800

    Settings

--- Shopee/Cell VCs/OrderCell.swift
+++ Shopee/Cell VCs/OrderCell.swift
@@ -19,6 +19,7 @@ class OrderCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Shopee/SettingsViewController.swift
+++ Shopee/SettingsViewController.swift
@@ -7,24 +7,36 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    let ref = Database.database().reference()
+    let uid = Auth.auth().currentUser?.uid
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        if emailTextField.text?.isEmpty == true || addressTextField.text?.isEmpty == true || phoneTextField.text?.isEmpty == true {
+            let alert = UIAlertController(title: "Error", message: "Please fill in the blanks.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            self.present(alert, animated: true, completion: nil)
+            } else {
+            ref.child("users/\(uid!)/email").setValue(emailTextField.text!)
+            ref.child("users/\(uid!)/address").setValue(addressTextField.text!)
+            ref.child("users/\(uid!)/phone").setValue(phoneTextField.text!)
+        }
     }
-    */
+    
 
 }
